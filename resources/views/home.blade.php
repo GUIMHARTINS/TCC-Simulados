7518ab68126d385a1020a00e1664473e651dc842
Andreia Cristiane Valentim de Lima <andreiacv.lima@gmail.com>
2021-09-10 21:34:52 -0300

    layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QUESTEST</title>
+    <link rel="stylesheet" href="/layout.css">
+</head>
+
+<body>
+    <header id="cabecalho">
+        <img src="/LOGO.png" alt="">
+        <input type="text" placeholder="Pesquisar Simulados...">
+    </header>
+    <section id="conteudo">
+
+    </section>
+    <footer id="rodape">
+        QUESTEST SIMULADOR 2021
+    </footer>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
-
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/simulado', function () {
+    return view('simulado');
+});
+Route::get('/resultados', function () {
+    return view('resultados');
 });
